9af0916445d3be8c9d13388434f2b7229e496325
ErdnaHog <49737083+ErdnaHog@users.noreply.github.com>
2020-06-18 14:47:01 +0800

    Allow everyone to use same database by using file path

--- App/__init__.py
+++ App/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_restful import Api
+import os
 
 app = Flask(__name__)
 api_app = Api(app)
 
+file_directory = os.path.dirname(os.path.dirname(__file__))
 
-
-from App.api.todoSimple import TodoSimple
-api_app.add_resource(TodoSimple, '/api')
+from App.api.todoSimple import Users
+api_app.add_resource(Users, '/api/user')
 
 from App.web import routes
--- App/api/todoSimple.py
+++ App/api/todoSimple.py
@@ -1,10 +1,16 @@
-from App import api
 from flask import request
 from flask_restful import Resource
+import sqlite3, os
+from App import file_directory
 
 todos = {}
 
-
-class TodoSimple(Resource):
+class Users(Resource):
     def get(self):
-        return "hi there"
+        conn = sqlite3.connect(os.path.join(file_directory,"storage.db"))
+        c = conn.cursor()
+        c.execute("SELECT * FROM users")
+        conn.commit()
+        user = c.fetchall()
+        conn.close()
+        print(user)
