1e4ad4bf60aac2e1db377c4a947230062ba76bd1
k3ith <k3ith.tang@gmail.com>
2020-06-19 22:42:46 +0800

    xm

--- flaskr/views/user.py
+++ flaskr/views/user.py
@@ -49,6 +49,7 @@ def signin():
         # ' or 1=1-- (login to admin account)
         # user'-- (login to any account)
         # ' or rowid=1-- (login to any account)
+        # ZAP' OR '1'='1' --
         c.execute("SELECT rowid, * FROM users WHERE username='{}' AND password='{}' ".format(signin.username.data, signin.password.data))
         conn.commit()
         user = c.fetchone()
