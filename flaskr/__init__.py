90bf734be200f20bead013a9e54f1bfbcdd567c0
Andre <andregohkr@hotmail.com>
2020-06-20 22:43:00 +0800

    User can retrieve password using api

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_login import LoginManager
 from flask_jwt_extended import JWTManager
+from flask_mail import Mail
 import os, sqlite3
 
 app = Flask(__name__)
@@ -9,14 +10,17 @@ app.config.from_object('config')
 api_app = Api(app)
 login_manager = LoginManager(app)
 jwt = JWTManager(app)
+mail = Mail(app)
 
 file_directory = os.path.dirname(os.path.dirname(__file__))
 
 from flaskr.api.users import Users
 from flaskr.api.login import Login
+from flaskr.api.retrieve_password import Retrieve_Password
 
 api_app.add_resource(Users, '/api/user')
 api_app.add_resource(Login, '/api/login')
+api_app.add_resource(Retrieve_Password, '/api/retrieve_password')
 
 from flaskr.models.User import User
 
