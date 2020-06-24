7ccdd68cb7663cab1f6b3cd37891071e244f848a
Andre <andregohkr@hotmail.com>
2020-06-25 01:56:49 +0800

    Creating new products is a success Modified Show_Product.html to contain all the new attributes of a product Modified Products.html to filter inactive and active products

--- flaskr/api/products.py
+++ flaskr/api/products.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify
+from flask import request, jsonify, redirect, url_for
 from flask_restful import Resource
 import sqlite3, os
 from flaskr import file_directory
@@ -13,20 +13,31 @@ class Products(Resource):
         products = c.fetchall()
         for product in products:
             product_list.append(
-                {"productName": product[0], "productSellingPrice": product[1], "productCostPrice": product[2], "status":product[3]})
+                {"productName": product[0], "productImage": product[1], "productDescription": product[2],
+                 "productSellingPrice": product[3], "productCostPrice": product[4], "productCategory": product[5],
+                 "status": product[6]})
         return jsonify(data=product_list)
 
     def post(self):
-        request_json_data = request.get_json(force=True)
+        product_name = request.form.get('productNameInput')
+        product_img = request.files.get('productImage')
+        product_description = request.form.get('productDescription')
+        product_selling_price = request.form.get('productSellingPrice')
+        product_cost_price = request.form.get('productCostPrice')
+        product_category = request.form.get('productCategory')
 
-        product_name = request_json_data['product_name']
-        product_selling_price = request_json_data['product_selling_price']
-        product_cost_price = request_json_data['product_cost_price']
+        filename = product_img.filename
+        filepath = 'products/' + filename
+        product_img.save(os.path.join(file_directory, 'flaskr/static/img/products', filename))
 
         conn = sqlite3.connect(os.path.join(file_directory, "storage.db"))
         c = conn.cursor()
-        c.execute(f"INSERT INTO products VALUES ('{product_name}', '{product_selling_price}', '{product_cost_price}', 'active')")
+        c.execute(
+            f"INSERT INTO products VALUES ('{product_name}', '{filepath}', '{product_description}', '{product_selling_price}', '{product_cost_price}', '{product_category}','active')")
         conn.commit()
+
+        if request.user_agent.string[:14] != "PostmanRuntime":
+            return redirect(url_for('admin.show_product'))
 
         return jsonify(data="Success")
 
